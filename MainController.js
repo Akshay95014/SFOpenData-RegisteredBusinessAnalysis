8d946e829a8326ba6b7a008f5301cddfbba97dcb
Akshay Aggarwal <akshay95014@gmail.com>
2016-09-21 10:50:54 -0700

    Added tables to breakdown businesses by (a) supervisory district, (b) industry, and (c) start year.

--- MainController.js
+++ MainController.js
@@ -9,12 +9,46 @@
             $scope.queryData = data;
         }
 
+        var onError = function() {
+            $scope.error = "Couldn't fetch the data";
+        }
+
         var getBusinessesByCity = function(city) {
             $log.debug("Made it into controller's getBusinessesByCity function.");
-            opendata.getLocationCityData(city).then(onGotData, function() {
-                $scope.error = "Couldn't fetch the data";
+            opendata.getLocationCityData(city).then(onGotData, onError);
+        }
+
+        var onBizByDistrict = function(data) {
+            $scope.businessByDistrict = data;
+
+            //This will convert all numeric strings to numbers
+            angular.forEach($scope.businessByDistrict, function(business) {
+                business.supervisor_district = parseFloat(business.supervisor_district);
+                business.count = parseFloat(business.count);
             });
         }
+
+        var onBizByIndustry = function(data) {
+            $scope.businessByIndustry = data;
+
+            //This will convert all numeric strings to numbers
+            angular.forEach($scope.businessByIndustry, function(business) {
+                business.count = parseFloat(business.count);
+            });
+        }
+
+        var onBizByYear = function(data) {
+            $scope.businessByYear = data;
+
+            //This will convert all numeric strings to numbers
+            angular.forEach($scope.businessByYear, function(business) {
+                business.count = parseFloat(business.count);
+            });
+        }
+
+        opendata.businessesByDistrict().then(onBizByDistrict, onError);
+        opendata.businessesByIndustry().then(onBizByIndustry, onError);
+        opendata.businessesByYear().then(onBizByYear, onError);
 
         $scope.getBusinessesByCity = getBusinessesByCity;
 
--- opendata.js
+++ opendata.js
@@ -2,6 +2,8 @@
 
   var opendata = function($http){
 
+    var appKey = "&$$app_token=Uw0ptivuIBN9JgGZLQtFZJ2In";
+
     var getLocationCityData = function(city){
       return $http.get("https://data.sfgov.org/resource/vbiu-2p9h.json?city=" + city + "&$$app_token=Uw0ptivuIBN9JgGZLQtFZJ2In")
            .then(function(response){
@@ -9,8 +11,36 @@
            });
     };
 
+    var businessesByDistrict = function(){
+      var queryString = "?$select=supervisor_district,%20count(*)&$group=supervisor_district";
+      return $http.get("https://data.sfgov.org/resource/vbiu-2p9h.json" + queryString + appKey)
+            .then(function(response){
+              return response.data;
+            });
+    };
+
+    var businessesByIndustry = function(){
+      var queryString = "?$select=naic_code_description,%20count(*)&$group=naic_code_description";
+      return $http.get("https://data.sfgov.org/resource/vbiu-2p9h.json" + queryString + appKey)
+            .then(function(response){
+              return response.data;
+            });
+    };
+
+    var businessesByYear = function(){
+      var queryString = "?$select=date_trunc_y(dba_start_date)%20as%20year,%20count(*)&$group=date_trunc_y(dba_start_date)";
+      return $http.get("https://data.sfgov.org/resource/vbiu-2p9h.json" + queryString + appKey)
+            .then(function(response){
+              return response.data;
+            });
+    };
+
+
     return {
       getLocationCityData: getLocationCityData,
+      businessesByDistrict: businessesByDistrict,
+      businessesByIndustry: businessesByIndustry,
+      businessesByYear: businessesByYear
     };
 
   };
